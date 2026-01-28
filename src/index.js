8606132b02ff42b85011828709c601eaeab271d0
Tu Phan <tuphan@gears.bot>
2026-01-28 08:35:54 +0300

    feat: add failover provider

---
+++ src/index.js
@@ -0,0 +1,152 @@
+/**
+ * @typedef {Object} FailoverProviderConfig
+ * @property {number} [retries] - The number of retries in the failover mechanism.
+ */
+
+/**
+ * @template T
+ * @typedef {Object} ProviderProxy<T>
+ * @property {T} provider - The provider abstraction.
+ * @property {number} ms - The last response duration, which is planned for quorum mechanism.
+ */
+
+/**
+ * @class
+ * @template {Object} T
+ * @type {FailoverProvider<T>} The failover factory
+ */
+export default class FailoverProvider {
+  /**
+   * @private
+   * @type {number} The current active provider index.
+   */
+  _activeProvider = 0;
+
+  /**
+   * @private
+   * @type {Array<ProviderProxy<T>>} The list of provider candidates.
+   */
+  _providers = [];
+
+  /**
+   * @private
+   * @type {number} The number of retries before the failover provider throws an error.
+   */
+  _retries = 3;
+
+  /**
+   * @param {FailoverProviderConfig} config - The failover factory config.
+   */
+  constructor({ retries = 3 } = {}) {
+    this._retries = retries;
+  }
+
+  /**
+   * Add a provider into the list of candidates
+   * @template {T} P
+   * @param {P} provider Provider
+   * @returns The instance of FailoverProvider
+   */
+  addProvider = (provider) => {
+    this._providers.push({ provider, ms: 0 });
+    return this;
+  };
+
+  /**
+   * The FailoverProvider factory
+   * @returns The instance of FailoverProvider
+   */
+  initialize = () => {
+    if (!this._providers.length)
+      throw new Error(
+        "Cannot initialize an empty provider. Call `addProvider` before this function.",
+      );
+
+    const [{ provider }] = this._providers;
+
+    return new Proxy(provider, {
+      get: (_, p, receiver) => {
+        return this.proxy(this._providers[this._activeProvider], p, receiver);
+      },
+    });
+  };
+
+  /**
+   * Switch to the next candidate provider by round robin
+   * @private
+   * @returns The new candidate provider
+   */
+  _switch = () => {
+    this._activeProvider = (this._activeProvider + 1) % this._providers.length;
+    return this._providers[this._activeProvider];
+  };
+
+  /**
+   * Store the response time of the latest request
+   * @private
+   * @param {ProviderProxy<T>} target - The provider proxy
+   * @returns The benchmark close function
+   */
+  _benchmark = (target) => {
+    const start = Date.now();
+    return () => {
+      target.ms = Math.round(Date.now() - start);
+    };
+  };
+
+  /**
+   * Proxy handler will keep retry until a response or throw the latest error.
+   * @private
+   * @param {ProviderProxy<T>} target The current active provider
+   * @param {string | symbol} p The method name
+   * @param {any} receiver The JS Proxy
+   * @param {number} retries The number of retries
+   * @returns
+   */
+  proxy = (target, p, receiver, retries = this._retries) => {
+    /**
+     * @param {...any} args
+     * @returns {any}
+     */
+    return (...args) => {
+      const record = this._benchmark(target);
+      /**
+       * @type {any | Promise<any>}
+       */
+      let re;
+
+      // Retry on sync functions
+      try {
+        const prop = Reflect.get(target.provider, p, receiver);
+        if (typeof prop !== "function") return prop;
+
+        re = prop.apply(target.provider, args);
+        if (!re?.then) {
+          record();
+          return re;
+        }
+      } catch (er) {
+        record();
+        if (retries <= 0) throw er;
+        return this.proxy(this._switch(), p, receiver, retries - 1);
+      }
+
+      // Retry on async functions
+      return re
+        .then(
+          /**
+           * @param {any} re
+           */
+          (re) => {
+            record();
+            return re;
+          },
+        )
+        .catch((er) => {
+          record();
+          if (retries <= 0) throw er;
+          return this.proxy(this._switch(), p, receiver, retries - 1)(...args);
+        });
+    };
+  };
+}
---
+++ types/index.d.ts
@@ -0,0 +1,91 @@
+/**
+ * @typedef {Object} FailoverProviderConfig
+ * @property {number} [retries] - The number of retries in the failover mechanism.
+ */
+/**
+ * @template T
+ * @typedef {Object} ProviderProxy<T>
+ * @property {T} provider - The provider abstraction.
+ * @property {number} ms - The last response duration, which is planned for quorum mechanism.
+ */
+/**
+ * @class
+ * @template {Object} T
+ * @type {FailoverProvider<T>} The failover factory
+ */
+export default class FailoverProvider<T extends unknown> {
+    /**
+     * @param {FailoverProviderConfig} config - The failover factory config.
+     */
+    constructor({ retries }?: FailoverProviderConfig);
+    /**
+     * @private
+     * @type {number} The current active provider index.
+     */
+    private _activeProvider;
+    /**
+     * @private
+     * @type {Array<ProviderProxy<T>>} The list of provider candidates.
+     */
+    private _providers;
+    /**
+     * @private
+     * @type {number} The number of retries before the failover provider throws an error.
+     */
+    private _retries;
+    /**
+     * Add a provider into the list of candidates
+     * @template {T} P
+     * @param {P} provider Provider
+     * @returns The instance of FailoverProvider
+     */
+    addProvider: <P extends T>(provider: P) => this;
+    /**
+     * The FailoverProvider factory
+     * @returns The instance of FailoverProvider
+     */
+    initialize: () => any;
+    /**
+     * Switch to the next candidate provider by round robin
+     * @private
+     * @returns The new candidate provider
+     */
+    private _switch;
+    activeProvider: any;
+    /**
+     * Store the response time of the latest request
+     * @private
+     * @param {ProviderProxy<T>} target - The provider proxy
+     * @returns The benchmark close
+     */
+    private _benchmark;
+    /**
+     * Proxy handler will keep retry until a response or throw the latest error.
+     * @private
+     * @param {ProviderProxy<T>} target The current active provider
+     * @param {string | symbol} p The method name
+     * @param {any} receiver The JS Proxy
+     * @param {number} retries The number of retries
+     * @returns
+     */
+    private proxy;
+}
+export type FailoverProviderConfig = {
+    /**
+     * - The number of retries in the failover mechanism.
+     */
+    retries?: number;
+};
+/**
+ * <T>
+ */
+export type ProviderProxy<T> = {
+    /**
+     * - The provider abstraction.
+     */
+    provider: T;
+    /**
+     * - The last response duration, which is planned for quorum mechanism.
+     */
+    ms: number;
+};
